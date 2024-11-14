f919ac5cc11fa2380ae5f39afd01bce1581da9e9
danushri-p <danushri.prakash@kalvium.community>
2024-11-14 15:48:15 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -9,34 +9,18 @@ function App() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [score, setScore] = useState(0);
   const [isQuizCompleted, setIsQuizCompleted] = useState(false);
-  const [feedback, setFeedback] = useState(null);
 
   // Function to handle when an option is clicked
   const optionClick = (isCorrect) => {
-    setFeedback(isCorrect ? "Correct!" : "Incorrect");
-
     if (isCorrect) {
       setScore(score + 1);
     }
 
     // Move to the next question or complete the quiz if last question is reached
     if (currentQuestion < questions.length - 1) {
-      setTimeout(() => {
-        setCurrentQuestion(currentQuestion + 1);
-        setFeedback(null); // Clear feedback for the next question
-      }, 1000); // Delay to show feedback
+      setCurrentQuestion(currentQuestion + 1);
     } else {
-      setTimeout(() => {
-        setIsQuizCompleted(true);
-      }, 1000); // Delay to show final feedback before ending quiz
-    }
-  };
-
-  // Function to go back to the previous question
-  const previousQuestion = () => {
-    if (currentQuestion > 0) {
-      setCurrentQuestion(currentQuestion - 1);
-      setFeedback(null); // Clear feedback when going back
+      setIsQuizCompleted(true);
     }
   };
 
@@ -45,11 +29,10 @@ function App() {
     setScore(0);
     setCurrentQuestion(0);
     setIsQuizCompleted(false);
-    setFeedback(null);
   };
 
   return (
-    <div>
+    <div className="app-container">
       <h3>Score: {score}</h3>
       {!isQuizCompleted ? (
         <>
@@ -59,10 +42,6 @@ function App() {
             query={currentQuestion}
             selectchoice={optionClick}
           />
-          {feedback && <p>{feedback}</p>}
-          <button onClick={previousQuestion} disabled={currentQuestion === 0}>
-            Previous
-          </button>
         </>
       ) : (
         <Result
